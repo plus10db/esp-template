829195e48031c4235a44fa1dacac1e87f673359d
Malachi Burke <malachi.burke@gmail.com>
2025-11-03 12:25:16 -0800

    feat: prepping interactive console

---
+++ test/devtool/main/console.cpp
@@ -0,0 +1,38 @@
+#include <esp_console.h>
+#include <esp_log.h>
+
+static const char* TAG = "devtool";
+
+esp_console_repl_t* init_repl()
+{
+    ESP_LOGI(TAG, "Initializing console");
+
+    esp_console_repl_t* repl = nullptr;
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    /* Prompt to be printed before each line.
+     * This can be customized, made dynamic, etc.
+     */
+    repl_config.prompt = "L&T>";
+    repl_config.max_cmdline_length = 80;
+
+    esp_console_register_help_command();
+
+    // Be careful, it's easy to botch up your ESP32S3 with incorrect configuration
+#if defined(CONFIG_ESP_CONSOLE_UART_DEFAULT) || defined(CONFIG_ESP_CONSOLE_UART_CUSTOM)
+    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+
+#elif defined(CONFIG_ESP_CONSOLE_USB_CDC)
+    esp_console_dev_usb_cdc_config_t hw_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&hw_config, &repl_config, &repl));
+#elif defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
+    esp_console_dev_usb_serial_jtag_config_t hw_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl));
+#else
+#error
+#endif
+
+    return repl;
+}
+
+
--- test/devtool/main/devtool.cpp
+++ test/devtool/main/devtool.cpp
@@ -1,6 +1,12 @@
+#include <esp_console.h>
+
 #include <lt-now.h>
+
+esp_console_repl_t* init_repl();
 
 extern "C" void app_main(void)
 {
+    init_repl();
+
     lt_init_espnow();
 }
